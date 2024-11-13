cd8e875801b83f96b61edf221bcdde02ec1793e2
gon <gongorgojo@gmail.com>
2024-11-13 15:20:58 -0300

    fixed map

--- src/components/locations.js
+++ src/components/locations.js
@@ -2,7 +2,7 @@ export default function Map() {
   return (
     <iframe
       id="mapa"
-      class="h-[60vh] w-[100vw] sm:h-[70vh]"
+      class="h-[60vh] w-[98vw] sm:h-[70vh]"
       title="Mapa"
       src="https://www.google.com/maps/d/embed?mid=1PBwZKGymflUYuJnIiwjIhMPkOkZf7upw&ehbc=2E312F"
     ></iframe>
